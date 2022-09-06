6318e3e4091bbb70abb2e500688d0a32b38a66fa
Thomas Ma <thomayinma@gmail.com>
2022-09-06 10:23:21 -0700

    added first iteration of custom model load supprot
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>

--- opensearch_py_ml/ml_commons_integration/ml_common_client.py
+++ opensearch_py_ml/ml_commons_integration/ml_common_client.py
@@ -5,7 +5,7 @@ SPDX-License-Identifier: Apache-2.0
 
 from opensearchpy import OpenSearch
 from opensearch_py_ml.ml_commons_integration.upload.ml_common_upload_client import MLCommonUploadClient, DEFAULT_ML_COMMON_UPLOAD_CHUNK_SIZE
-
+from opensearch_py_ml.ml_commons_integration.load.ml_common_load_client import MLCommonLoadClient
 
 class MLCommonClient:
     """
@@ -16,6 +16,7 @@ class MLCommonClient:
     def __init__(self, os_client: OpenSearch):
         self._client = os_client
         self._upload_client = MLCommonUploadClient(os_client)
+        self._load_client = MLCommonLoadClient(os_client)
 
     def put_model(self,
                   model_path: str,
@@ -24,3 +25,6 @@ class MLCommonClient:
                   chunk_size: int = DEFAULT_ML_COMMON_UPLOAD_CHUNK_SIZE,
                   verbose: bool = False) -> None:
         self._upload_client.put_model(model_path, model_name, version_number, chunk_size, verbose)
+
+    def load_model(self, model_name: str, version_number: int):
+        return self._load_client.load_model(model_name, version_number)
