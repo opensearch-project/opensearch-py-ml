4802e6982c4d8c04509008f1ce6b57ef5efdeeb9
Dhrubo Saha <dhrubo@amazon.com>
2022-09-28 19:24:01 -0700

    refactoring

--- opensearch_py_ml/ml_commons_integration/ml_common_utils.py
+++ opensearch_py_ml/ml_commons_integration/ml_common_utils.py
@@ -1,6 +1,8 @@
-"""
-Copyright OpenSearch Contributors
-SPDX-License-Identifier: Apache-2.0
- """
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Any modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
 
 ML_BASE_URI = '/_plugins/_ml'
