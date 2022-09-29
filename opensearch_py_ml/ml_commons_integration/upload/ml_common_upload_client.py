1995a65897ec60dec6ebeb707584d4bf8a4b5bba
Dhrubo Saha <dhrubo@amazon.com>
2022-09-28 19:24:01 -0700

    refactoring
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>

--- opensearch_py_ml/ml_commons_integration/upload/ml_common_upload_client.py
+++ opensearch_py_ml/ml_commons_integration/upload/ml_common_upload_client.py
@@ -1,7 +1,9 @@
-"""
-Copyright OpenSearch Contributors
-SPDX-License-Identifier: Apache-2.0
- """
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Any modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
 
 from opensearchpy import OpenSearch
 import os
