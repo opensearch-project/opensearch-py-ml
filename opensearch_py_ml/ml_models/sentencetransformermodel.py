e0d17501c10086a867fcbd15a0cb5eb192b88eba
Yerzhaisang <55043014+Yerzhaisang@users.noreply.github.com>
2023-07-20 00:07:43 +0600

    Enabled auto-truncation for any pretrained models (#192)
    * Made truncation parameter automatically processed
    
    Signed-off-by: Yerzhaisang Taskali <tasqali1697@gmail.com>
    
    * Made max_length parameter dynamic
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Added unit test for checking truncation parameter
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Updated CHANGELOG.md
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Included the test of max_length parameter value
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Slightly modeified the test of max_length parameter value
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Modified CHANGELOG.md and removed the duplicate
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Enabled auto-truncation format also for ONNX
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Implemented reusable function
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Fixed the lint
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Change tokenizer.json only if truncation is null
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Removed function which had been accidentally added
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Renamed reusable function and added the description
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    * Fixed the lint
    
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>
    
    ---------
    
    Signed-off-by: Yerzhaisang Taskali <tasqali1697@gmail.com>
    Signed-off-by: yerzhaisang <tasqali1697@gmail.com>

--- opensearch_py_ml/ml_models/sentencetransformermodel.py
+++ opensearch_py_ml/ml_models/sentencetransformermodel.py
@@ -701,6 +701,37 @@ class SentenceTransformerModel:
             )
         print("zip file is saved to " + zip_file_path + "\n")
 
+    def _fill_null_truncation_field(
+        self,
+        save_json_folder_path: str,
+        max_length: int,
+    ) -> None:
+        """
+        Description:
+        Fill truncation field in tokenizer.json when it is null
+
+        :param save_json_folder_path:
+             path to save model json file, e.g, "home/save_pre_trained_model_json/")
+        :type save_json_folder_path: string
+        :param max_length:
+             maximum sequence length for model
+        :type max_length: int
+        :return: no return value expected
+        :rtype: None
+        """
+        tokenizer_file_path = os.path.join(save_json_folder_path, "tokenizer.json")
+        with open(tokenizer_file_path) as user_file:
+            parsed_json = json.load(user_file)
+        if "truncation" not in parsed_json or parsed_json["truncation"] is None:
+            parsed_json["truncation"] = {
+                "direction": "Right",
+                "max_length": max_length,
+                "strategy": "LongestFirst",
+                "stride": 0,
+            }
+            with open(tokenizer_file_path, "w") as file:
+                json.dump(parsed_json, file, indent=2)
+
     def save_as_pt(
         self,
         sentences: [str],
@@ -760,6 +791,9 @@ class SentenceTransformerModel:
 
         # save tokenizer.json in save_json_folder_name
         model.save(save_json_folder_path)
+        self._fill_null_truncation_field(
+            save_json_folder_path, model.tokenizer.model_max_length
+        )
 
         # convert to pt format will need to be in cpu,
         # set the device to cpu, convert its input_ids and attention_mask in cpu and save as .pt format
@@ -851,6 +885,9 @@ class SentenceTransformerModel:
 
         # save tokenizer.json in output_path
         model.save(save_json_folder_path)
+        self._fill_null_truncation_field(
+            save_json_folder_path, model.tokenizer.model_max_length
+        )
 
         convert(
             framework="pt",
