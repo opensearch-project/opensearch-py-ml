a1fed762c2292ddfa670b0b8063bdbc75aef709a
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-08-01 17:42:20 -0700

    Edit python file to get data from config
    Signed-off-by: Thanawan Atchariyachanvanit <latchari@amazon.com>

--- utils/model_uploader/update_pretrained_model_listing.py
+++ utils/model_uploader/update_pretrained_model_listing.py
@@ -8,14 +8,7 @@
 import argparse
 import json
 import os
-import shutil
-import sys
-
-THIS_DIR = os.path.dirname(__file__)
-ROOT_DIR = os.path.join(THIS_DIR, "../..")
-sys.path.append(ROOT_DIR)
-
-from opensearch_py_ml.ml_models.sentencetransformermodel import SentenceTransformerModel
+from typing import Optional
 
 JSON_FILENAME = "pretrained_model_listing.json"
 JSON_DIRNAME = "utils/model_uploader/model_listing"
@@ -31,70 +24,55 @@ TEST_SENTENCES = [
 ]
 
 
-def get_sentence_transformer_model_description(model_name, model_format) -> str:
+def get_sentence_transformer_model_description(
+    config_folder_name: str, config_filepath: str
+) -> Optional[str]:
     """
-    Get description of the pretrained sentence transformer model
+    Get description of the pretrained sentence transformer model from config file
 
-    :param model_name: Model name of the pretrained model
-    (e.g. huggingface/sentence-transformers/msmarco-distilbert-base-tas-b)
-    :type model_name: string
-    :param model_format: Model format of the pretrained model (TORCH_SCRIPT/ONNX)
-    :type model_format: string
+    :param config_folder_name: Name of the local folder that stores config files (e.g. 'config_folder')
+    :type config_folder_name: string
+    :param config_filepath: Path to local config file
+    (e.g. 'sentence-transformers/all-MiniLM-L12-v2/2.0.0/onnx/config.json')
+    :type config_filepath: string
     :return: Description of the model
-    :rtype: string
+    :rtype: string or None
     """
-    model_id = model_name[len("huggingface/") :]
-    pre_trained_model = SentenceTransformerModel(
-        model_id=model_id, folder_path=TEMP_MODEL_PATH, overwrite=True
-    )
-    if model_format == TORCH_SCRIPT_FORMAT:
-        pre_trained_model.save_as_pt(model_id=model_id, sentences=TEST_SENTENCES)
-    else:
-        pre_trained_model.save_as_onnx(model_id=model_id)
-
-    description = None
-    readme_file_path = os.path.join(TEMP_MODEL_PATH, "README.md")
-    if os.path.exists(readme_file_path):
-        try:
-            description = pre_trained_model.get_model_description_from_md_file(
-                readme_file_path
-            )
-        except Exception as e:
-            print(f"Cannot get model description from README.md file: {e}")
+    filepath = os.path.join(config_folder_name, config_filepath)
     try:
-        shutil.rmtree(TEMP_MODEL_PATH)
+        with open(filepath, "r") as f:
+            model_config = json.load(f)
     except Exception as e:
-        assert False, f"Raised Exception while deleting {TEMP_MODEL_PATH}: {e}"
-    return description
+        print(f"Cannot open {filepath} to get model description: {e}")
+        return None
+    if "description" in model_config:
+        return model_config["description"]
+    else:
+        return None
 
 
-def create_new_pretrained_model_listing(models_txt_filename, old_json_filename):
+def create_new_pretrained_model_listing(
+    config_paths_txt_filename: str, config_foldername: str, old_json_filename: str
+):
     """
     Create a new pretrained model listing and store it at PRETRAINED_MODEL_LISTING_JSON_FILEPATH
     based on current models in models_txt_filename and the old pretrained model
     listing in old_json_filename
 
-    :param models_txt_filename: Name of the txt file that stores string of
-    directories in the ml-models/huggingface/ folder of the S3 bucket
-    :type models_txt_filename: string
-    :param old_json_filename: Name of the json file that contains the old pretrained
-    model listing
+    :param config_paths_txt_filename: Name of the txt file that stores paths to config file
+    in the ml-models/huggingface/ folder of the S3 bucket
+    :type config_paths_txt_filename: string
+    :param config_foldername: Name of the local folder that stores config files
+    :type config_foldername: string
+    :param old_json_filename: Name of the json file that stores outdated model listing
     :type old_json_filename: string
     :return: No return value expected
     :rtype: None
     """
     print("\n=== Begin running update_pretrained_model_listing.py ===")
-    print(f"--- Reading {models_txt_filename} ---")
-    with open(models_txt_filename, "r") as f:
-        model_lst = f.read().split()
-        model_lst = list(
-            set(
-                [
-                    model_filepath[: model_filepath.rfind("/")]
-                    for model_filepath in model_lst
-                ]
-            )
-        )
+    print(f"--- Reading {config_paths_txt_filename} ---")
+    with open(config_paths_txt_filename, "r") as f:
+        config_paths_lst = f.read().split()
 
     print(f"--- Reading {old_json_filename} --- ")
     with open(old_json_filename, "r") as f:
@@ -106,56 +84,61 @@ def create_new_pretrained_model_listing(models_txt_filename, old_json_filename):
 
     print("---  Creating New Model Listing --- ")
     new_model_listing_dict = {}
-    for model_filepath in model_lst:
-        if model_filepath.startswith(PREFIX_SENTENCE_TRANSFORMER_FILEPATH):
-            # (e.g. ml-models/huggingface/sentence-transformers/msmarco-distilbert-base-tas-b/1.0.1/torch_script)
-            model_parts = model_filepath.split("/")
+    for config_filepath in config_paths_lst:
+        if config_filepath.startswith(PREFIX_SENTENCE_TRANSFORMER_FILEPATH):
+            # (e.g. 'ml-models/huggingface/sentence-transformers/all-MiniLM-L12-v2/2.0.0/onnx/config.json')
+            model_parts = config_filepath.split("/")
             model_name = "/".join(model_parts[1:4])
             model_version = model_parts[4]
             model_format = model_parts[5]
+            local_config_filepath = model_parts[2:]
             if model_name not in new_model_listing_dict:
                 new_model_listing_dict[model_name] = {
                     "name": model_name,
                     "version": [],
                     "format": [],
                 }
+                if model_name in old_model_listing_dict:
+                    if "description" in old_model_listing_dict[model_name]:
+                        new_model_listing_dict[model_name][
+                            "description"
+                        ] = old_model_listing_dict[model_name]["description"]
+                else:
+                    description = get_sentence_transformer_model_description(
+                        config_foldername, local_config_filepath
+                    )
+                    if description is not None:
+                        new_model_listing_dict[model_name]["description"] = description
             model_content = new_model_listing_dict[model_name]
             if model_version not in model_content["version"]:
                 model_content["version"].append(model_version)
             if model_format not in model_content["format"]:
                 model_content["format"].append(model_format)
-            if model_name in old_model_listing_dict:
-                if "description" in old_model_listing_dict[model_name]:
-                    model_content["description"] = old_model_listing_dict[model_name][
-                        "description"
-                    ]
-            else:
-                try:
-                    description = get_sentence_transformer_model_description(
-                        model_name, model_format.upper()
-                    )
-                except Exception as e:
-                    description = None
-                    print(f"Cannot get sentence transformer model description: {e}")
-                if description is not None:
-                    model_content["description"] = description
 
     new_model_listing_lst = list(new_model_listing_dict.values())
     new_model_listing_lst = sorted(new_model_listing_lst, key=lambda d: d["name"])
 
-    print(f"---  Dumping New Model Listing in {PRETRAINED_MODEL_LISTING_JSON_FILEPATH} --- ")
+    print(
+        f"---  Dumping New Model Listing in {PRETRAINED_MODEL_LISTING_JSON_FILEPATH} --- "
+    )
     if not os.path.isdir(JSON_DIRNAME):
         os.makedirs(JSON_DIRNAME)
     with open(PRETRAINED_MODEL_LISTING_JSON_FILEPATH, "w") as f:
         json.dump(new_model_listing_lst, f, indent=1)
     print("\n=== Finished running update_pretrained_model_listing.py ===")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "models_txt_filename",
+        "config_paths_txt_filename",
         type=str,
-        help="Name of the file that stores model names",
+        help="Name of the file that stores config paths in S3",
+    )
+    parser.add_argument(
+        "config_foldername",
+        type=str,
+        help="Name of the local folder that stores copies of config files from S3",
     )
     parser.add_argument(
         "old_json_filename",
@@ -165,11 +148,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if not args.models_txt_filename.endswith(
+    if not args.config_paths_txt_filename.endswith(
         ".txt"
     ) or not args.old_json_filename.endswith(".json"):
         assert False, "Invalid arguments"
 
     create_new_pretrained_model_listing(
-        args.models_txt_filename, args.old_json_filename
+        args.config_paths_txt_filename,
+        args.local_config_foldername,
+        args.old_json_filename,
     )
