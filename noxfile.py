ff0a68fafbc0c3261b55846e78f2a78c734fa50b
Thanawan Atchariyachanvanit <latchari@amazon.com>
2023-08-18 10:59:01 -0700

    Update noxfile.py
    Signed-off-by: Thanawan Atchariyachanvanit <latchari@amazon.com>

--- noxfile.py
+++ noxfile.py
@@ -125,8 +125,6 @@ def test(session, pandas_version: str):
         "-m",
         "pytest",
         "--cov-report=term-missing",
-        "--cov=opensearch_py_ml/",
-        "--cov=utils/model_uploader/update_pretrained_model_listing.py",
         "--cov-config=setup.cfg",
         "--doctest-modules",
         "--nbval",
