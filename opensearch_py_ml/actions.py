b8b652b23df2c61587101ab4d87a3b5b7819fab0
Vaishnavi Pasumarthi <53794971+vaipos@users.noreply.github.com>
2023-09-21 10:00:38 -0500

    optimize actions.py
    Signed-off-by: Vaishnavi Pasumarthi <53794971+vaipos@users.noreply.github.com>

--- opensearch_py_ml/actions.py
+++ opensearch_py_ml/actions.py
@@ -24,13 +24,11 @@
 #  under the License.
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Dict, List, Optional, Union
+from typing import List, Optional, Union, Dict
+
+import pandas as pd
 
 from opensearch_py_ml import SortOrder
-
-if TYPE_CHECKING:
-    import pandas as pd  # type: ignore
-
 
 class PostProcessingAction(ABC):
     def __init__(self, action_type: str) -> None:
@@ -49,74 +47,91 @@ class PostProcessingAction(ABC):
         return self._action_type
 
     @abstractmethod
-    def resolve_action(self, df: "pd.DataFrame") -> "pd.DataFrame":
+    def resolve_action(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
     @abstractmethod
     def __repr__(self) -> str:
         pass
 
-
 class SortIndexAction(PostProcessingAction):
     def __init__(self, items: Optional[Union[List[int], List[str]]] = None) -> None:
+        """
+        SortIndexAction constructor
+
+        Parameters
+        ----------
+        items : Optional[Union[List[int], List[str]]]
+            The items to sort by index
+        """
         super().__init__("sort_index")
         self._items = items
 
-    def resolve_action(self, df: "pd.DataFrame") -> "pd.DataFrame":
-        if self._items is not None:
-            return df.reindex(self._items)
-        else:
-            return df.sort_index()
+    def resolve_action(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.reindex(self._items) if self._items else df.sort_index()
 
     def __repr__(self) -> str:
         return f"('{self.type}')"
 
-
 class HeadAction(PostProcessingAction):
     def __init__(self, count: int) -> None:
-        super().__init__("head")
+        """
+        HeadAction constructor
 
+        Parameters
+        ----------
+        count : int
+            The number of rows to include from the start
+        """
+        super().__init__("head")
         self._count = count
 
-    def resolve_action(self, df: "pd.DataFrame") -> "pd.DataFrame":
+    def resolve_action(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.head(self._count)
 
     def __repr__(self) -> str:
         return f"('{self.type}': ('count': {self._count}))"
 
-
 class TailAction(PostProcessingAction):
     def __init__(self, count: int) -> None:
-        super().__init__("tail")
+        """
+        TailAction constructor
 
+        Parameters
+        ----------
+        count : int
+            The number of rows to include from the end
+        """
+        super().__init__("tail")
         self._count = count
 
-    def resolve_action(self, df: "pd.DataFrame") -> "pd.DataFrame":
+    def resolve_action(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.tail(self._count)
 
     def __repr__(self) -> str:
         return f"('{self.type}': ('count': {self._count}))"
 
-
 class SortFieldAction(PostProcessingAction):
     def __init__(self, sort_params: Dict[str, str]) -> None:
+        """
+        SortFieldAction constructor
+
+        Parameters
+        ----------
+        sort_params : Dict[str, str]
+            The sorting parameters as a dictionary (e.g., {'_doc': 'desc'})
+        """
         super().__init__("sort_field")
-
-        if sort_params is None:
-            raise ValueError("Expected valid dictionary")
-
-        # Split string
-        sort_field, sort_order = list(sort_params.items())[0]
+        sort_field, sort_order = next(iter(sort_params.items()), ('', ''))
         if not sort_field or sort_order not in ("asc", "desc"):
             raise ValueError(
                 f"Expected ES sort params dictionary (e.g. {{'_doc': 'desc'}}). Got '{sort_params}'"
             )
-
         self._sort_field = sort_field
         self._sort_order = SortOrder.from_string(sort_order)
 
-    def resolve_action(self, df: "pd.DataFrame") -> "pd.DataFrame":
-        return df.sort_values(self._sort_field, self._sort_order == SortOrder.ASC)
+    def resolve_action(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.sort_values(self._sort_field, ascending=self._sort_order == SortOrder.ASC)
 
     def __repr__(self) -> str:
         return f"('{self.type}': ('sort_field': '{self._sort_field}', 'sort_order': {self._sort_order}))"
