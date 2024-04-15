085c00824acfe9100da0c025f7df771494db5508
kalyanr <kalyan.ben10@live.com>
2024-04-16 05:27:29 +0530

    update validators
    Signed-off-by: kalyanr <kalyan.ben10@live.com>

--- opensearch_py_ml/ml_commons/validators.py
+++ opensearch_py_ml/ml_commons/validators.py
@@ -103,3 +103,17 @@ def validate_profile_input(path_parameter, payload):
 
     if payload is not None and not isinstance(payload, dict):
         raise ValueError("payload needs to be a dictionary or None")
+
+
+def validate_stats_input(node_id, stat_id, payload):
+    if payload:
+        if node_id or stat_id:
+            raise ValueError(
+                "Stats API does not accept node_id or stat_id with payload"
+            )
+    if payload is not None and not isinstance(payload, dict):
+        raise ValueError("payload needs to be a dictionary or None")
+    if node_id is not None and not isinstance(node_id, str):
+        raise ValueError("node_id needs to be a string or None")
+    if stat_id is not None and not isinstance(stat_id, str):
+        raise ValueError("stat_id needs to be a string or None")
