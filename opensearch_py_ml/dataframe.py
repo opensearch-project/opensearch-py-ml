d493529097e2ee8e44c55741b59fee284717ebe5
Yerzhaisang Taskali <yerzhaisang@Yerzhaisangs-MacBook-Pro.local>
2024-10-07 11:15:41 +0500

    Replaced deprecated method _construct_axes_from_arguments
    Signed-off-by: Yerzhaisang Taskali <yerzhaisang@Yerzhaisangs-MacBook-Pro.local>

--- opensearch_py_ml/dataframe.py
+++ opensearch_py_ml/dataframe.py
@@ -424,9 +424,7 @@ class DataFrame(NDFrame):
             axis = pd.DataFrame._get_axis_name(axis)
             axes = {axis: labels}
         elif index is not None or columns is not None:
-            axes, _ = pd.DataFrame()._construct_axes_from_arguments(
-                (index, columns), {}
-            )
+            axes = {"index": pd.Index(index), "columns": pd.Index(columns)}
         else:
             raise ValueError(
                 "Need to specify at least one of 'labels', 'index' or 'columns'"
