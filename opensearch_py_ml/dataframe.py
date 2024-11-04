c68b7f8e543865b510001a0016b9ed9157046308
Yerzhaisang Taskali <tasqali1697@gmail.com>
2024-11-04 13:58:52 +0500

    removed unused comment
    Signed-off-by: Yerzhaisang Taskali <tasqali1697@gmail.com>

--- opensearch_py_ml/dataframe.py
+++ opensearch_py_ml/dataframe.py
@@ -424,9 +424,6 @@ class DataFrame(NDFrame):
             axis = pd.DataFrame._get_axis_name(axis)
             axes = {axis: labels}
         elif index is not None or columns is not None:
-            # axes, _ = pd.DataFrame()._construct_axes_from_arguments(
-            #     (index, columns), {}
-            # )
             axes = {}
             if index is not None:
                 if isinstance(index, pd.Index):
