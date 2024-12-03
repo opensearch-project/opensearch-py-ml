4503a55108f43de23a52af166aabe23a9d7b8a5d
hmumtazz <144855436+hmumtazz@users.noreply.github.com>
2024-12-02 20:02:03 -0800

    fixed license header rag.py
    Signed-off-by: hmumtazz <144855436+hmumtazz@users.noreply.github.com>

--- opensearch_py_ml/ml_commons/rag_pipeline/rag/rag.py
+++ opensearch_py_ml/ml_commons/rag_pipeline/rag/rag.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-# SPDX-License-Identifier: Apache-2.0
+#!/usr/bin/env python3  # SPDX-License-Identifier: Apache-2.0
 # The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
@@ -20,10 +18,9 @@
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
+# KIND, either express or implied.  See the License for
+# the specific language governing permissions and limitations
 # under the License.
-
 """
 Main CLI script for OpenSearch PY ML
 """
